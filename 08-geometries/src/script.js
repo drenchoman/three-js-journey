566f669ccd2ca8d4dd8c25e86f3aa0d4550e3eb5
Oscar Harron <oscarharron@hotmail.com>
2024-03-08 19:44:07 +1300

    testing bufferattribute

--- 08-geometries/src/script.js
+++ 08-geometries/src/script.js
@@ -11,7 +11,7 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BufferGeometry();
-const count = 100;
+const count = 50;
 const positionsArray = new Float32Array(count * 3 * 3);
 
 for (let i = 0; i < count * 3 * 3; i++) {
@@ -27,12 +27,17 @@ geometry.setAttribute('position', positionsAttribute);
 // Object
 // const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
 const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
+  color: 0xfeef00,
   wireframe: true,
 });
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const mesh2 = new THREE.Mesh(geometry, material);
 
+scene.add(mesh);
+mesh.position.x = 2;
+scene.add(mesh2);
+mesh2.position.x = -2;
+// mesh2.material.color = 0xfff000;
 // Sizes
 const sizes = {
   width: window.innerWidth,
@@ -60,7 +65,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.z = 3;
+camera.position.z = 10;
 scene.add(camera);
 
 // Controls
@@ -80,7 +85,13 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  geometry.rotateX;
+  mesh.rotation.x = (elapsedTime * Math.PI) / 10;
+  mesh2.rotation.x = (elapsedTime * Math.PI) / -10;
+  // mesh.rotation.y = Math.cos(elapsedTime * 0.5);
+  mesh.position.y = Math.sin(elapsedTime * 1);
+  mesh2.position.y = Math.cos(elapsedTime * 1);
+  mesh2.position.x = elapsedTime;
+  mesh2.position.x = Math.cos(elapsedTime * 1);
   // Update controls
   controls.update();
 
