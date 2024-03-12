fe9885a68cf7f980a159cf2a5f98b2c2ce2414a1
Oscar Harron <oscarharron@hotmail.com>
2024-03-13 08:52:24 +1300

    continue gui options

--- 09-debug-ui/src/script.js
+++ 09-debug-ui/src/script.js
@@ -4,9 +4,21 @@ import gsap from 'gsap';
 import GUI from 'lil-gui';
 
 // Debug
-const gui = new GUI();
+const gui = new GUI({
+  width: 340,
+  title: 'Debug UI',
+  closeFolders: false,
+});
+gui.hide();
+window.addEventListener('keydown', (event) => {
+  if (event.key == 'h') {
+    gui.show(gui._hidden);
+  }
+});
 const debugObject = {};
 
+const cubeTweaks = gui.addFolder('Awesome Cube');
+// cubeTweaks.close();
 /**
  * Base
  */
@@ -21,6 +33,8 @@ const scene = new THREE.Scene();
  */
 
 debugObject.color = '#b86161';
+debugObject.subdivision = 2;
+
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
 const material = new THREE.MeshBasicMaterial({
   color: debugObject.color,
@@ -28,18 +42,38 @@ const material = new THREE.MeshBasicMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-gui
+cubeTweaks
   .add(mesh.position, 'y')
   .min(-3)
   .max(3)
   .step(0.01)
   .name('elevation');
 
-gui.add(mesh, 'visible');
-gui.add(material, 'wireframe');
-gui.addColor(debugObject, 'color').onChange((event) => {
+cubeTweaks.add(mesh, 'visible');
+cubeTweaks.add(material, 'wireframe');
+cubeTweaks.addColor(debugObject, 'color').onChange((event) => {
   material.color.set(debugObject.color);
 });
+
+cubeTweaks
+  .add(debugObject, 'subdivision')
+  .min(1)
+  .max(20)
+  .step(1)
+  .onFinishChange(() => {
+    // Remove old geometries
+    mesh.geometry.dispose();
+
+    // Create new one
+    mesh.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      debugObject.subdivision,
+      debugObject.subdivision,
+      debugObject.subdivision
+    );
+  });
 
 debugObject.spin = () => {
   gsap.to(mesh.rotation, {
@@ -48,7 +82,7 @@ debugObject.spin = () => {
   });
 };
 
-gui.add(debugObject, 'spin');
+cubeTweaks.add(debugObject, 'spin');
 //
 
 /**
